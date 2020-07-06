7eed279d31141b27e8cac43c0f90b9985a72f9a4
3d3d3d-code <reda.hamza8@orange.fr>
2020-07-06 07:52:26 +0200

    exo

---
+++ exo js/afficher des variables/index.js
@@ -0,0 +1,11 @@
+var nom = "Nom : " + "toto";
+var age = "Age :" + 30;
+var homme =  true;
+
+console.log("Nom :" + nom);
+console.log("Age : " + age);
+if (homme === true) {
+    console.log("Sexe : Homme ");
+} else {
+    console.log("Sexe : femme");
+}
---
+++ exo js/inversion valeurs/test.js
@@ -0,0 +1,15 @@
+var a = 1;
+var b = 5;
+
+
+console.log("------------Avant inversion---------");
+console.log("A:" + a);
+console.log("B:" + b);
+ 
+
+var tmp = a;
+a = b;
+b = tmp;
+console.log ("---------------Apres inversion-------------");
+console.log("A : " + a);
+console.log("B : " + b);
