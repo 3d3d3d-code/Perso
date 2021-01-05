09a1652f0ae8e0aff272ebf31276638618c84e25
3d3d3d-code <reda.hamza8@orange.fr>
2021-01-05 07:34:17 +0100

    tech_io

--- tutoriaux php/champagne/index.php
+++ tutoriaux php/champagne/index.php
@@ -1,4 +1,5 @@
 <?php
+
 class Mere
 {
     protected $_name;
@@ -9,9 +10,30 @@ class Mere
 
     public function hello()
     {
-        echo 'Coucou , je suis la mere : '.this->_name;
+        echo 'Coucou , je suis la mere : ' . $this->_name.'<br>';
     }
 }
 
+class Fille extends Mere
+{
+    private  $_otherName;
+     public function __construct($name,$otherName){
+         parent::__construct($name);
+         $this->_otherName = $otherName;
+     }
+
+     public function hello() 
+     {
+         echo  'je suis la fille : ' .$this->_name . ' ' .$this->_otherName.'<br>';
+     }
+}
+
+
+
 $mama = new Mere('Katie');
 $mama->hello();
+
+$fifille = new Fille('Maude',' LANISSIE ');
+$fifille->hello();
+
+?>
